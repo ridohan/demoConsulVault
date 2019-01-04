6a7be7093ce4ef1f64fa0ea2a597420fc3ce3398
ridohan <red.khiter@gmail.com>
2019-01-04 17:53:57 +0100

    Added Vault Support for read / write secrets

--- src/main/java/com/ridohan/example/consul/demo/ConsulVaultDemoApplication.java
+++ src/main/java/com/ridohan/example/consul/demo/ConsulVaultDemoApplication.java
@@ -1,9 +1,13 @@
 package com.ridohan.example.consul.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.vault.core.VaultTemplate;
+import org.springframework.vault.support.VaultResponse;
+import org.springframework.vault.support.VaultResponseSupport;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class ConsulVaultDemoApplication   {
 
 
+    @Autowired
+    private VaultTemplate vaultTemplate;
+
+
     @Value("${foo}")
     private String foo;
 
+    @Value("${password}")
+    String password;
 
     public static void main(String[] args) {
         SpringApplication.run(ConsulVaultDemoApplication.class, args);
@@ -26,6 +36,21 @@ public class ConsulVaultDemoApplication   {
     @GetMapping("/foo")
     public String getFoo() {
         return foo;
+    }
+
+    @GetMapping("/password")
+    public String getPassword() {
+        return password;
+    }
+
+
+    @GetMapping("/writeAndRetrieveCredentials")
+    public String writeAndRetrieveCredentials() {
+        Credentials credentials = new Credentials("username", "password");
+        vaultTemplate.write("secret/test",credentials);
+        VaultResponseSupport<Credentials> response = vaultTemplate.read("secret/test",Credentials.class);
+        Credentials retrievedCredentials =  response.getData();
+        return retrievedCredentials.toString();
     }
 
 
